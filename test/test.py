8cc456beafaa3a6cd97371aec7818d7a0b6221fe
ndroi <1083859361@qq.com>
2016-08-26 12:05:35 +0800

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,6 +5,6 @@ print t.data["info"]["name"].decode("utf-8")
 files=t.data["info"]["files"]
 for item in files:
     print item["path"][0].decode("utf-8")
-t.data["info"]["name"]="my_name" #change info. some software mayread ["info"]["name.utf-8"]
+t.data["info"]["name"]="my_name" #change info. some software may read ["info"]["name.utf-8"]
 t.dump("dump.torrent")  #the new torrent file
 
